23980d1af85eb7bd61c53adee590df70f64fd6ca
Scantron445 <scanlonianm21@gmail.com>
2023-04-21 12:03:50 -0400

    display qr codes for each chair

--- src/app/(admin)/admin/(orders)/table-order/[toid]/page.tsx
+++ src/app/(admin)/admin/(orders)/table-order/[toid]/page.tsx
@@ -4,6 +4,7 @@ import { getRestaurantInAdmin } from '@/graphql/services/restaurant.server'
 import { getTableOrder } from '@/graphql/services/table-order.server'
 import { getCurrentSessionUser } from '@/lib/session'
 import { Accordion } from '@ui/main/data-display/accordion/Accordion'
+import { uuid } from '@zag-js/utils'
 import React from 'react'
 import QRCode from "react-qr-code"
 
@@ -14,7 +15,7 @@ export default async function Page({ params: { toid } }: { params: { toid: strin
    
    const restaurant = await getRestaurantInAdmin(sessionUser?.id)
    
-   const tableOrder = getTableOrder(toid)
+   const tableOrder = await getTableOrder(toid)
    
    return (
       <>
@@ -25,15 +26,20 @@ export default async function Page({ params: { toid } }: { params: { toid: strin
             
             <Accordion>
                <Accordion.Item title="Show QR Codes">
-                  <div className="mt-2 bg-gray-50 p-4">
-                     <div className="w-60 relative mx-auto p-8 border border-2 bg-white rounded-xl">
-                        <QRCode
-                           size={256}
-                           style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-                           value={`${clientEnv.NEXT_PUBLIC_BASE_URL}/r/${restaurant.slug}/table/`}
-                           viewBox={`0 0 256 256`}
-                        />
-                     </div>
+                  <div className="mt-2 bg-gray-50 p-4 space-y-4 md:space-y-0 md:flex">
+                     {Array.from(Array(tableOrder?.table?.no_of_chairs ?? 0).keys()).map(chair_index => {
+                        return (
+                           <div key={chair_index + uuid()} className="w-60 relative mx-auto p-8 border border-2 bg-white rounded-xl flex-shrink-0">
+                              <p className="text-center w-full -mt-4 mb-4 text-lg font-medium">Chair {chair_index+1}</p>
+                              <QRCode
+                                 size={256}
+                                 style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                                 value={`${clientEnv.NEXT_PUBLIC_BASE_URL}/r/${restaurant.slug}/table/${tableOrder?.id}_${chair_index+1}`}
+                                 viewBox={`0 0 256 256`}
+                              />
+                           </div>
+                        )
+                     })}
                   </div>
                </Accordion.Item>
             </Accordion>
