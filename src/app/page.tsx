7294e077c8d30e94fa98450cd8c0ac7b55bcdda3
5rahim <bonfohzaki@gmail.com>
2023-05-09 20:48:33 -0400

    dormant branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import { useTranslation } from '@/app/i18n'
-import { getCurrentSessionUser } from '@/lib/session'
-
 export default async function Page({ params: { lng } }: { params: { lng: string } }) {
    
    return (
